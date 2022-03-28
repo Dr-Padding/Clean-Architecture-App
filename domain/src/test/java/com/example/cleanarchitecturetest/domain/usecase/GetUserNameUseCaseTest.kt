a07161607d1a5fc45af43bd184e2058805dc0db3
Azamat Zarkeshev <dr.padding@Azamats-MBP.Dlink>
2022-03-28 12:18:10 +0600

    unit tests started

---
+++ domain/src/test/java/com/example/cleanarchitecturetest/domain/usecase/GetUserNameUseCaseTest.kt
@@ -0,0 +1,16 @@
+package com.example.cleanarchitecturetest.domain.usecase
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class GetUserNameUseCaseTest {
+
+    @Test
+    fun test1() {
+
+        val expected = 4
+        val actual = 2 + 2 + 1
+        Assertions.assertEquals(expected, actual)
+    }
+
+}
